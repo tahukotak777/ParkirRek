226385b6b5fadf3214b69047da7ed3c9c31c590b
IkhwanVanno <ikhwanvanno750@gmail.com>
2024-08-22 06:44:29 +0700

    menambah beberapa element

--- resources/views/keluar.blade.php
+++ resources/views/keluar.blade.php
@@ -5,34 +5,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Keluar Kendaraan</title>
+    <script src="https://unpkg.com/html5-qrcode/minified/html5-qrcode.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5-qrcode/2.1.4/html5-qrcode.min.js"></script>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
     <header>
         <h1>PARKIR REKK!</h1>
     </header>
-
-    <nav>
-        <a href="/" class="button">Masuk Kendaraan</a>
-        <a href="/keluar" class="button">Keluar Kendaraan</a>
-    </nav>
-
-    <main>
-        <form id="form-keluar" action="/keluar/parkir" method="post">
-            <label for="barcodeMasuk">Scan Barcode Masuk:</label>
-            <input type="text" id="barcodeMasuk" name="barcodeMasuk" required>
-
-            <label for="sntk">STNK Kendaraan:</label>
-            <input type="text" id="sntk" name="sntk" required>
-
-            <button type="submit">Hitung Biaya</button>
-        </form>
-
-        <div id="biayaParkir">
-            <!-- Tempat menampilkan biaya parkir -->
-        </div>
-    </main>
-
+    <!-- gk kepakek -->
+     
     <script src="js/keluar.js"></script>
 </body>
 </html>
